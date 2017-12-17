0f9599688fd27d6afae2e3fbee8e86d1da840645
Fisher <544473461@qq.com>
2017-12-17 16:17:09 +0800

    改为在index.blade配置店助权限

--- resources/assets/front/attendance/index.vue
+++ resources/assets/front/attendance/index.vue
@@ -487,14 +487,6 @@
                     this.shopDutyStaffKey = key;
                     this.shopDutyActions = [];
                     //@TODO 使用一段时间后恢复店助全开
-                    let assistantActive = [
-                        'lsw4084', 'lsw4026', 'lsw3859',
-                        'lsc1894', 'lsw3862', 'lsw3766',
-                        'lsw3977', 'lsc1720', 'lsc1947',
-                        'lsc1911', 'lsw4032', 'lsw3630',
-                        'lsw4128', 'lsw4112',
-                    ];
-
                     if (staffAttendance.shop_duty_id == 3 && assistantActive.indexOf(this.attendanceData.shop_sn) != -1) {
                         this.shopDutyActions.push({name: '设为店助', method: this.setShopDutyToAssistant});
                     } else if (staffAttendance.shop_duty_id == 2) {
